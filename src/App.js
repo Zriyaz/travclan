49ed1aa3f6b861391ecc5b416b351a775568c10d
= <=>
2021-06-27 00:31:07 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import HomeIcon from "@material-ui/icons/Home";
+import IconButton from "@material-ui/core/IconButton";
+
 import { Context } from "./context";
+import Logo from "./assets/logo.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  image: {
+    width: "200px",
+    height: "70px",
+  },
+  navBackground: {
+    backgroundColor: "#394e89",
+  },
+  IconButton: {
+    marginLeft: "auto",
+  },
+  homeIcon: {
+    fontSize: "45px",
+    color: "#fdbe4b",
+  },
+}));
 
 const App = () => {
-  return <div>Hello</div>;
+  const classes = useStyles();
+  const { state, dispatch } = useContext(Context); // Accessing Global State of the Application using useContext hook, It returns the application state as well as dispatch to update the global state.
+
+  // First application render in the DOM useEffect hook will execute and fetch data from remote server
+  useEffect(() => {
+    fetch("https://intense-tor-76305.herokuapp.com/merchants").then(
+      (res) =>
+        res.json().then((data) => dispatch({ type: "GET_DATA", payload: data })) // After fetched data from remote server dispatch the action called "GET_DATA" and  It will store data in global store of the application and accessible in all components.
+    );
+  }, []);
+
+  console.log("Data", state);
+  return (
+    <div>
+      <AppBar position="static" className={classes.navBackground}>
+        <Toolbar>
+          <img src={Logo} className={classes.image} alt="travclan" />
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            className={classes.IconButton}
+          >
+            <HomeIcon className={classes.homeIcon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default App;
